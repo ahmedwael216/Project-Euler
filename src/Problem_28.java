6539ffd4faff26175956cdc64003d783f7b7e0cf
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-10 08:19:16 +0200

    question 28 done

--- src/Problem_28.java
+++ src/Problem_28.java
@@ -3,6 +3,10 @@ package src;
 
 public class Problem_28 {
     public static void main(String[] args) {
+        int dimension = 1001;
+        int n= (dimension-1)/2;
 
+        int sol = (3 + 2 * n * ( 8*n*n + 15*n + 13)) / 3;
+        System.out.println(sol);
     }
 }
