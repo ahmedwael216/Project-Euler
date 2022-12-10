ba3f2cde98381b23d2ce64ba1a1922cb88f2196f
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-10 08:16:54 +0200

    question 27 restored

--- src/Problem_27.java
+++ src/Problem_27.java
@@ -3,6 +3,37 @@ package src;
 
 public class Problem_27 {
     public static void main(String[] args) {
-
+        int maxa=Integer.MIN_VALUE;
+        int maxb=Integer.MIN_VALUE;
+        int maxNumberOfPrimes =0;
+        for (int i = -999; i < 1000; i++) {
+            for (int j = -999; j < 1000 ; j++) {
+                int val = 2;
+                int n=-1;
+                while(isPrime(val)){
+                    val = n*n + i*n + j;
+                    n++;
+                }
+                if(n>maxNumberOfPrimes){
+                    maxa=i;
+                    maxb=j;
+                    maxNumberOfPrimes=n;
+                }
+            }
+        }
+        System.out.println(maxa*maxb);
     }
+
+    public static  boolean isPrime(int num){
+        if(num<2){
+            return false;
+        }
+        for(int i=2;i * i <=num;i++){
+            if((num%i)==0){
+                return  false;
+            }
+        }
+        return true;
+    }
+
 }
---
+++ src/Problem_28.java
@@ -0,0 +1,8 @@
+package src;
+
+
+public class Problem_28 {
+    public static void main(String[] args) {
+
+    }
+}
