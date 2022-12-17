b8c649ac1f5aca3e4955ea713264dc8cba9c62f2
Ahmed Wael <AhmedWael216@protonmail.com>
2022-12-17 08:56:50 +0200

    problem 35 done

--- src/Problem_34.java
+++ src/Problem_34.java
@@ -2,7 +2,7 @@ package src;
 public class Problem_34 {
     public static int[] factorials = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
     public static void main(String[] args) {
-        var sum = 0;
+        int sum = 0;
         for (var i = 10; i <= 1499999; i++) {
             if (i == factorialSum(i)) {
                 sum+= i;
